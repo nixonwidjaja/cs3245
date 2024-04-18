388d37bb3e1fcac48b430bf923a9d7ef6f4d10c3
EvitanRelta <shauntanzongzhi@gmail.com>
2024-04-18 15:03:46 +0800

    Add function to get cached tokenized content

--- HW4/dataset.py
+++ HW4/dataset.py
@@ -1,6 +1,11 @@
 import csv
+import os
+import pickle
 import sys
 from typing import Iterator, TypedDict
+
+from preprocessor import Preprocessor
+from tqdm.autonotebook import tqdm
 
 
 class DataElement(TypedDict):
@@ -24,6 +29,8 @@ def _set_csv_limit_to_max():
 
 class Dataset:
     """Handles the loading of dataset."""
+
+    CACHE_FILE_PATH = "cached_content_tokens.pkl"
 
     @staticmethod
     def load_dataset_stream(dataset_path: str) -> Iterator[DataElement]:
@@ -87,3 +94,48 @@ class Dataset:
                 }
 
             yield last_element
+
+    @staticmethod
+    def get_tokenized_content_list(
+        dataset_path: str,
+        save_cache: bool = False,
+        validate_cache: bool = True,
+    ) -> list[tuple[int, list[str]]]:
+        """Get list of `(Doc-ID, token_list)` tuples, where `token_list` is a
+        tokens of the document's `"content"`.
+
+        If a precomputed cache of the tokens list exists, load the cache instead.
+
+        Args:
+            dataset_path (str): Path to the CSV dataset file.
+            save_cache (bool, optional): Whether to cache the tokens list after computing. \
+                Defaults to True.
+            validate_cache (bool, optional): Whether to validate the cached tokens list \
+                (if it exists). Defaults to True.
+        """
+        # Return cache if exists.
+        if os.path.exists(Dataset.CACHE_FILE_PATH):
+            with open(Dataset.CACHE_FILE_PATH, "rb") as f:
+                tokens_list = pickle.load(f)
+
+            if validate_cache:
+                # Validate that every 500th element matches the cache, to ensure the cache is correct.
+                dataset = list(Dataset.load_dataset_stream(dataset_path))
+                assert len(dataset) == len(tokens_list)
+                for element, (doc_id, tokens) in zip(dataset[::500], tokens_list[::500]):
+                    assert doc_id == element["document_id"]
+                    assert tokens == list(Preprocessor.to_token_stream(element["content"]))
+
+            return tokens_list
+
+        dataset = list(Dataset.load_dataset_stream(dataset_path))
+        tokens_list: list[tuple[int, list[str]]] = []
+        for element in tqdm(dataset):
+            tokens = list(Preprocessor.to_token_stream(element["content"]))
+            tokens_list.append((element["document_id"], tokens))
+
+        if save_cache:
+            with open(Dataset.CACHE_FILE_PATH, "wb") as f:
+                pickle.dump(tokens_list, f)
+
+        return tokens_list
