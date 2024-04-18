d41505547ccf8d602a45faa04c9f42fc72b643aa
EvitanRelta <shauntanzongzhi@gmail.com>
2024-04-18 22:23:20 +0800

    Add query expansion feat.

--- HW4/search.py
+++ HW4/search.py
@@ -9,6 +9,7 @@ from functools import cmp_to_key
 import nltk
 from indexer import Indexer
 from preprocessor import Preprocessor
+from query_expansion import expand_query
 
 
 def usage():
@@ -52,7 +53,9 @@ def run_search(
     # Compute scores.
     with Indexer(dict_path, postings_path) as indexer:
         N = indexer.num_docs
-        tf_dict = nltk.FreqDist(Preprocessor.to_token_stream(query))
+        query_tokens = list(Preprocessor.to_token_stream(query))
+        query_tokens = expand_query(query_tokens)
+        tf_dict = nltk.FreqDist(query_tokens)
 
         # Compute un-normalized scores first.
         query_norm_length = 0
